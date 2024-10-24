8f37acaa243db7e0f3803408ed54d96aa539153b
KnightofInd <rs1448@srmist.edu.in>
2024-10-24 08:43:37 +0530

    initial d

---
+++ smart/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
+
+function App() {
+  return (
+ 
+      <>
+      <Navbar />
+      <HeroSection />
+      <Footer />
+      </>
+  );
+}
+
+export default App;
